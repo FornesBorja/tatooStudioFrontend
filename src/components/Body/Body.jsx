0bb746f774d9961ceaf4f8c1b80daf29c3bd7a52
FornesBorja <fornes.borja@gmail.com>
2024-07-24 15:34:29 +0200

    feat: App.jsx configured with routes.

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,8 +1,15 @@
+import { Routes, Route } from "react-router-dom";
 import React from 'react'
+import { NotFound } from '../../view/NotFound/NotFound';
+import { Home } from "../../view/Home/Home";
 
 export const Body = () => {
   return (
     <>
+    <Routes>
+        <Route path="*" element={<NotFound />}/>
+        <Route path="/" element={<Home />}/>
+      </Routes>
     </>
   )
 }
