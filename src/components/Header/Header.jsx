cb3c72815e7c2702c6d60232b2bbecb8b3748d53
FornesBorja <fornes.borja@gmail.com>
2024-07-26 12:47:05 +0200

    feat: baic nav bar created

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import { Surfer } from '../Surfer/Surfer';
 
 export const Header = () => {
+  const navigate = useNavigate();
+
+  const passport = JSON.parse(localStorage.getItem("passport"));
+  let token;
+  if (passport) {
+    token = passport.token;
+  }
+
   return (
     <>
-      
+      <div className="flex justify-space-betwwen">
+        <Surfer path="/" content="Home" />
+        <Surfer path="/services" content="Services" />
+        {token ? (
+          <>
+            <div className="log-button" onClick={() => localStorage.removeItem("passport")}> LOGOUT </div>
+            <Surfer path="/profile" content="Profile" />
+          </>
+        ) : (
+          <>
+            <div onClick={() => navigate("/login")}> Login </div>
+            <Surfer path="/register" content="Register" />
+          </>
+        )}
+      </div>
     </>
-
-  )
+  );
 }
